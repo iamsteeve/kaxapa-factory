63ae0d1ea05c247d278a1dc0e89cc4dc0ad56510
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2018-12-10 09:42:38 -0500

    Versión con Materialize

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import './styles/index.scss';
 import * as SmoothScroll  from 'smooth-scroll'
 import * as $ from 'jquery';
-import 'popper.js';
+import * as M from 'materialize-css';
+import * as Aos from 'aos';
 function requireAll(r:any) { r.keys().forEach(r); }
+
 // @ts-ignore
-requireAll(require.context('./img/', true));
+requireAll(require.context('./images/', true));
 
 const scroll = new SmoothScroll('[data-scroll]',{
     header: null,
@@ -15,6 +17,15 @@ const scroll = new SmoothScroll('[data-scroll]',{
 
 
 $(function () {
+    Aos.init();
+    const sidenav = document.querySelectorAll('.sidenav');
+    const instanceSidenav = M.Sidenav.init(sidenav, {});
+
+    const parallax = document.querySelectorAll('.parallax');
+    const instanceParallax = M.Parallax.init(parallax, {});
+
+    const select = document.querySelectorAll('select');
+    const instanceSelect = M.FormSelect.init(select,{})
 
 
 });
